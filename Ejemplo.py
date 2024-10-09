a5af1bddbb3e57689bc6e4b3c4a29842d590e84a
DylierUD <dsmoyat@udistrital.edu.co>
2024-10-09 09:53:35 -0500

    ahora sale la eleccion del contrincante aleatoria

--- Ejemplo.py
+++ Ejemplo.py
@@ -1,11 +1,16 @@
-# papel = ganar a piedra 
-# else:
-#     puede perder contra tijera 
-#     print(ganaste)
-
+import random
 
 eleccion = input("Piedra papel o tijera: ")
-contrincante = input("Piedra papel o tijera: ")
+contrincante = random.randint(1,3)
+
+if contrincante == 1:
+    contrincante = "papel"
+elif contrincante== 2:
+     contrincante= "piedra"
+else:
+    contrincante= "tijera"
+      
+print(eleccion,"vs", contrincante)
 
 if eleccion == "papel" and contrincante == "piedra" or contrincante == "papel" and eleccion == "piedra":
     print("Gano papel")
